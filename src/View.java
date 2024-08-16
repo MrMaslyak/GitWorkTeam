fde5e2ccafafbbe460a80634f3b48a37d01411c3
Ilya Maslyanyi <maslanyjila@gmail.com>
2024-08-16 12:01:50 +0300

    first commit

---
+++ src/View.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class View extends JFrame {
+    public View() {
+        setTitle("Tic Tac Toe");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 300);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        ui();
+    }
+
+    private void ui() {
+    }
+
+}
