a50cc9757123cb4184db18f97bcc35319462d76b
igoryarmolyuk <igor.yarmolyuk00@gmail.com>
2024-08-16 12:07:58 +0300

    test

---
+++ src/Hero.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class Hero extends JFrame {
+    public Hero() {
+
+    }
+}
--- src/View.java
+++ src/View.java
@@ -11,6 +11,7 @@ public class View extends JFrame {
     }
 
     private void ui() {
+
     }
 
 }
